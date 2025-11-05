834cefffa3ab0469f1ef988774e37c07b70a57ab
Robani Diansyah <melinting522@gmail.com>
2025-11-05 19:45:56 +0700

    last update

--- mobile/app/barang/index.tsx
+++ mobile/app/barang/index.tsx
@@ -1,6 +1,6 @@
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, FlatList } from 'react-native'
 import React, { useEffect, useState } from 'react'
-import { FAB, TextInput } from 'react-native-paper'
+import { Button, Card, FAB, TextInput } from 'react-native-paper'
 import { Colors } from '@/constants/color'
 import { Feather } from "@expo/vector-icons";
 import axios from 'axios'
@@ -18,7 +18,7 @@ export default function BarangViewPage() {
 
     // buat fungsi untuk ambil data barang (GET)
     const getData = async () => {
-        await axios.get('http://10.35.115.72:3001/api/barang')
+        await axios.get('http://10.122.250.72:3001/api/barang')
         .then(function (response) {
             // console.log(response.data.barang);
             setData(response.data.barang);
@@ -50,11 +50,36 @@ export default function BarangViewPage() {
                 </View>
 
                 {/* komponen card */}
-                <Text>
+                {/* <Text>
                     {data.map((item, index) => (
-                        <Text key={index.id}>{item.harga}></Text>
+                        <Text key={index.id}>{item.satuan}></Text>
                     ))}
-                </Text>
+                </Text> */}
+
+                {/* {data.map((item, index) => ( */}
+                <FlatList
+                    data={data}
+                    style={{ width: "100%", backgroundColor: Colors.sponsor }}
+                    keyExtractor={(item) => item.id.toString()}
+                    contentContainerStyle={{ padding: 10 }}
+                    renderItem={({ item }) => (
+                        <Card key={item.id} style={styles.card}>
+                            <Card.Title
+                                title={item.nama}
+                                subtitle={item.harga}
+                            />
+                            <Card.Actions>
+                                <Button style={styles.card_button_primary}>
+                                    <Feather name="trash" size={20} color="white" onPress={() => console.log('delete')} />
+                                </Button>
+                                <Button style={styles.card_button_secondary}>
+                                    <Feather name="edit" size={20} color="blue" onPress={() => console.log('edit')} />
+                                </Button>
+                            </Card.Actions>
+                        </Card>
+                    )}
+                />
+                {/* ))} */}
 
                 {/* komponen FAB */}
             <FAB
@@ -92,4 +117,16 @@ const styles = StyleSheet.create({
         backgroundColor: Colors.sponsor,
         borderRadius: 35,
     },
-})
+    card: {
+        width: '90%',
+        margin: 10,
+    },
+    card_button_primary: {
+        backgroundColor: Colors.sponsor,
+        color: Colors.white,
+    },
+    card_button_secondary: {
+        color: Colors.white,
+        backgroundColor: Colors.white,
+    },
+});
